014d1f5fbd7ea616fd828f9dc5063dfb8dd2ca25
HAYASAKA_Ryosuke <hayasaka.ryosuke@gmail.com>
2014-01-30 18:02:50 +0900

    add antennatest configtest config antenna list

---
+++ antenna.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+import unittest
+import serial
+
+class RAC805:
+    def __init__(self):
+        pass
+
+    def moveazel(self,az,el):
+        command = "AZ"+str(az)+" EL"+str(el)+"\n\r"
+        return True
+
+    def stop(self):
+        command = "\n\r"
+        return True
+
+    def recieveenable(self):
+        readdata=">> "
+        return ">>" in readdata
+
+    def close(self):
+        return True
+
+class Antenna(object):
+    def __init__(self,rotatormodel):
+        if rotatormodel == "RAC805":
+            self._radio = RAC805()
+    def moveazel(self,az,el):
+        return self._radio.moveazel(az,el)
+    def stop(self):
+        return self._radio.stop()
+    def recieveenable(self):
+        return self._radio.recieveenable()
+    def close(self):
+        return self._radio.close()
+
+class testAntenna(unittest.TestCase):
+    def testcommand(self):
+        ant = Antenna("RAC805")
+        az=0.0
+        el=0.0
+        self.assertTrue(ant.moveazel(az,el))
+        self.assertTrue(ant.stop())
+        self.assertTrue(ant.recieveenable())
+        self.assertTrue(ant.close())
+    def testmoveantenna(self):
+        ant = Antenna("RAC805")
+        az=0.0
+        el=0.0
+        self.assertTrue(ant.moveazel(az,el))
+        self.assertTrue(ant.stop())
+        self.assertTrue(ant.close())
+        
+unittest.main()
--- config.py
+++ config.py
@@ -3,6 +3,7 @@
 import ConfigParser
 import unittest
 
+
 class ConfigRead(object):
     __lat = ''
     __lon = ''
@@ -12,25 +13,29 @@ class ConfigRead(object):
     __antenabaudrate = ''
     __radioport = ''
     __radiobaudrate = ''
+
     def __init__(self):
         config = ConfigParser.SafeConfigParser()
         config.read('config.ini')
-        self.__lat = config.get('GroundStation','Lat')
-        self.__lon = config.get('GroundStation','Lon')
-        self.__height = config.get('GroundStation','Height')
-        self.__radio = config.get('GroundStation','Radio')
-        self.__antenaport = config.get('GroundStation','AntenaPort')
-        self.__antenabaudrate = config.get('GroundStation','AntenaBaudRate')
-        self.__radioport = config.get('GroundStation','RadioPort')
-        self.__radiobaudrate = config.get('GroundStation','RadioBaudRate')
-        
-    def read(self,param=None):
+        self.__lat = config.get('GroundStation', 'Lat')
+        self.__lon = config.get('GroundStation', 'Lon')
+        self.__height = config.get('GroundStation', 'Height')
+        self.__radio = config.get('GroundStation', 'Radio')
+        self.__antenna= config.get('GroundStation', 'Antenna')
+        self.__antenaport = config.get('GroundStation', 'AntenaPort')
+        self.__antenabaudrate = config.get('GroundStation', 'AntenaBaudRate')
+        self.__radioport = config.get('GroundStation', 'RadioPort')
+        self.__radiobaudrate = config.get('GroundStation', 'RadioBaudRate')
+
+    def read(self, param=None):
         if param == 'lat':
             return self.__lat
         if param == 'lon':
             return self.__lon
         if param == 'height':
             return self.__height
+        if param == 'antenna':
+            return self.__antenna
         if param == 'radio':
             return self.__radio
         if param == 'antenaport':
@@ -41,28 +46,31 @@ class ConfigRead(object):
             return self.__radioport
         if param == 'radiobaudrate':
             return self.__radiobaudrate
-        if param == None:
-            return {'lat':self.__lat,'lon':self.__lon,'height':self.__height,'radio':self.__radio,'antenaport':self.__antenaport,'antenabaudrate':self.__antenabaudrate,'radioport':self.__radioport,'radiobaudrate':self.__radiobaudrate}
+        if param is None:
+            return {'lat': self.__lat, 'lon': self.__lon,'height':self.__height,'antenna': self.__antenna, 'radio': self.__radio, 'antenaport': self.__antenaport, 'antenabaudrate': self.__antenabaudrate, 'radioport': self.__radioport, 'radiobaudrate': self.__radiobaudrate}
+
 
 class testconfig(unittest.TestCase):
     def testconfig(self):
         config = ConfigRead()
-        self.assertEqual('43.134694',config.read('lat'))
-        self.assertEqual('141.248194',config.read('lon'))
-        self.assertEqual('50',config.read('height'))
-        self.assertEqual('IC910',config.read('radio'))
-        self.assertEqual('/dev/tty.USB',config.read('antenaport'))
-        self.assertEqual('9600',config.read('antenabaudrate'))
-        self.assertEqual('/dev/tty.USB2',config.read('radioport'))
-        self.assertEqual('38400',config.read('radiobaudrate'))
+        self.assertEqual('43.134694', config.read('lat'))
+        self.assertEqual('141.248194', config.read('lon'))
+        self.assertEqual('50', config.read('height'))
+        self.assertEqual('RAC805', config.read('antenna'))
+        self.assertEqual('IC910', config.read('radio'))
+        self.assertEqual('/dev/tty.USB', config.read('antenaport'))
+        self.assertEqual('9600', config.read('antenabaudrate'))
+        self.assertEqual('/dev/tty.USB2', config.read('radioport'))
+        self.assertEqual('38400', config.read('radiobaudrate'))
         data = config.read()
-        self.assertEqual('43.134694',data['lat'])
-        self.assertEqual('141.248194',data['lon'])
-        self.assertEqual('50',data['height'])
-        self.assertEqual('IC910',data['radio'])
-        self.assertEqual('/dev/tty.USB',data['antenaport'])
-        self.assertEqual('9600',data['antenabaudrate'])
-        self.assertEqual('/dev/tty.USB2',data['radioport'])
-        self.assertEqual('38400',data['radiobaudrate'])
+        self.assertEqual('43.134694', data['lat'])
+        self.assertEqual('141.248194', data['lon'])
+        self.assertEqual('RAC805', data['antenna'])
+        self.assertEqual('50', data['height'])
+        self.assertEqual('IC910', data['radio'])
+        self.assertEqual('/dev/tty.USB', data['antenaport'])
+        self.assertEqual('9600', data['antenabaudrate'])
+        self.assertEqual('/dev/tty.USB2', data['radioport'])
+        self.assertEqual('38400', data['radiobaudrate'])
 
 unittest.main()
