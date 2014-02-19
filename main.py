185fd9ccee8494bd62dd22a2e30bcf3ab2c5d3cf
HITGS <hayasaka.ryosuke@gmail.com>
2014-02-19 19:50:15 +0900

    create kivy

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 # -*- coding: utf-8 -*-
 
-import Orbitcalc
-import radio
-import antenna
-import config
-import unittest
+import kivy
+kivy.require('1.8.0')
 
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.clock import Clock
+
+class SampleWidget(Button):
+    
+    pass
+
+class MyApp(App):
+
+    def build(self):
+        root = SampleWidget()
+
+if __name__ == '__main__':
+    MyApp().run()
