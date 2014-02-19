8b13a9649b5c7e35e7601f61d0097742025878a9
kuroneko_HP <hayasaka.ryosuke@gmail.com>
2014-02-19 23:35:32 +0900

    bugfix Orbitcalctest.py

--- Orbitcalctest.py
+++ Orbitcalctest.py
@@ -2,11 +2,15 @@
 
 import unittest
 import Orbitcalc
+import math
+import ephem
+import datetime
+
 
 class testOrbitCalc(unittest.TestCase):
 
     def testOrbitcalc(self):
-        orbitinfo = Orbitcalc(gslat='43', gslon='141', gselev='50')
+        orbitinfo = Orbitcalc.Orbitcalc(gslat='43', gslon='141', gselev='50')
         orbitinfo.SatInfo('iss',
                       '1 99999U          13323.51230604  .00000000  00000-0  00000-0 0 00001',
                       '2 99999 051.5826 058.9447 0003029 020.0291 190.6000 15.51011028000098',
@@ -17,7 +21,7 @@ class testOrbitCalc(unittest.TestCase):
         print satfreq
 
     def testinputparamcheck(self):
-        orbitinfo = Orbitcalc(gslat=43, gslon=141, gselev=50)
+        orbitinfo = Orbitcalc.Orbitcalc(gslat=43, gslon=141, gselev=50)
         orbitinfo.SatInfo('iss',
                       '1 99999U          13323.51230604  .00000000  00000-0  00000-0 0 00001',
                       '2 99999 051.5826 058.9447 0003029 020.0291 190.6000 15.51011028000098',
@@ -28,5 +32,4 @@ class testOrbitCalc(unittest.TestCase):
         print satfreq
 
 if __name__ == "__main__":
-    print calctime_utc
     unittest.main()
