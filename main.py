93565d057b498787a97580acf4ed208f92175560
kuroneko_HP <hayasaka.ryosuke@gmail.com>
2014-02-21 03:38:02 +0900

    edit my.kv

--- main.py
+++ main.py
@@ -4,17 +4,20 @@ import kivy
 kivy.require('1.8.0')
 
 from kivy.app import App
-from kivy.uix.button import Button
-from kivy.clock import Clock
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
-class SampleWidget(Button):
-    
-    pass
+class SampleWidget(Widget):
+    buttonOperation = ObjectProperty(None)
+    def printvalue(self):
+        print self.buttonOperation.text
 
 class MyApp(App):
 
     def build(self):
         root = SampleWidget()
+        root.printvalue()
+        return root
 
 if __name__ == '__main__':
     MyApp().run()
