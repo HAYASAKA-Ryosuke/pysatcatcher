82058aea81d3e8ae49f1aa4d233d3b2821b75275
HITGS <hayasaka.ryosuke@gmail.com>
2014-02-22 00:56:02 +0900

    main.py callback function

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ kivy.require('1.8.0')
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
+from kivy.clock import Clock
+import Orbitcalc
+import antenna
+import config
+import radio
+import threading
 
 class SampleWidget(Widget):
     labelAOS = ObjectProperty(None)
@@ -15,18 +21,24 @@ class SampleWidget(Widget):
     labelfreq = ObjectProperty(None)
     labelstatus = ObjectProperty(None)
     buttonOperation = ObjectProperty(None)
-    def printvalue(self):
-        print self.buttonOperation.text
-        print self.labelAZ.text
 
     def buttonOperation_clicked(self,src):
-        print "click!"
+        if self.buttonOperation.text=="operation":
+            self.buttonOperation.text="close"
+            self.labelAOS.text="AOS:01:00:00"
+        else:
+            self.buttonOperation.text="operation"
+            self.labelAOS.text="AOS:02:00:00"
 class MyApp(App):
+    def callback_operation(self,dt):
+        if self.root.buttonOperation.text=="close":
+           self.root.labelAOS.text="hi"
 
     def build(self):
-        root = SampleWidget()
-        root.buttonOperation.bind(on_press=root.buttonOperation_clicked)
-        return root
+        self.root = SampleWidget()
+        self.root.buttonOperation.bind(on_press=self.root.buttonOperation_clicked)
+        Clock.schedule_interval(self.callback_operation,1)
+        return self.root
 
 
 if __name__ == '__main__':
