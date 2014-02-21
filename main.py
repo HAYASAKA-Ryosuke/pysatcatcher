2aac411895d0c8361d7cccb8dc5e7ee5db03eb9c
HITGS <hayasaka.ryosuke@gmail.com>
2014-02-21 19:05:37 +0900

    kivy design layout

--- main.py
+++ main.py
@@ -8,9 +8,15 @@ from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 
 class SampleWidget(Widget):
+    labelAOS = ObjectProperty(None)
+    labelLOS = ObjectProperty(None)
+    labelAZ = ObjectProperty(None)
+    labelEL = ObjectProperty(None)
+    labelstatus = ObjectProperty(None)
     buttonOperation = ObjectProperty(None)
     def printvalue(self):
         print self.buttonOperation.text
+        print self.labelAZ.text
 
 class MyApp(App):
 
